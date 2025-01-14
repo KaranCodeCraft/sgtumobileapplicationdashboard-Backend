757cc3f288101a2bca193c5316579a301d0271a7
Dev Karan <88235329+KaranCodeCraft@users.noreply.github.com>
2025-01-14 19:01:40 +0530

    added veiw document function

--- controllers/document.Controller.js
+++ controllers/document.Controller.js
@@ -1,11 +1,21 @@
 const Student = require("../models/Student");
 const path = require("path");
 const fs = require("fs");
+const {z} = require("zod");
+
+// Define the schema for the document upload request
+const documentUploadSchema = z.object({
+  studentId: z.string().nonempty(),
+  docName: z
+    .enum(["aadhar", "photo", "pan", "interMarksheet"]),
+});
 
 const documentupload = async (req, res) => {
     try {
-        const { studentId, docName } = req.body;
-       
+      const { studentId, docName } = req.body;
+      // Validate the request body
+        documentUploadSchema.parse({ studentId, docName });
+
         // Validate if a file was uploaded
         if (!req.file) {
             return res.status(400).json({
@@ -38,11 +48,34 @@ const documentupload = async (req, res) => {
         res.status(200).json({message: "Document uploaded successfully"});
     } catch (error) {
         console.log(error);
+        if(error.errors) {
+            return res.status(400).json({message: error.errors[0].message});
+        }
         // fs.unlinkSync(newFilePath);
         res.status(500).json({message: "Failed to upload document. Please try again later."});
     }
 }
 const documentview = async (req, res) => {
-    res.status(200).json({message: "Document View successfully"});
+  try {
+    const { studentId, docName } = req.body;
+    // Validate the request body
+    documentUploadSchema.parse({ studentId, docName });
+    const student = await Student.findById(studentId);
+    if (!student) {
+      return res.status(404).json({ message: "Student not found" });
+    }
+    const documentPath = student.document[docName];
+    if (!documentPath) {
+      return res.status(404).json({ message: "Document not found" });
+    }
+    const document = fs.readFileSync(documentPath);
+    res.contentType("application/pdf");
+    res.send(document);
+  } catch (error) {
+    console.log(error);
+    if (error.errors) {
+      return res.status(400).json({ message: error.errors[0].message });
+    } 
+  }
 }
 module.exports = { documentupload, documentview };
